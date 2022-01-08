85ed76e785c76aed5a9db199a036ec8eb19c5f08
Arcadeuh <fouque.bastien@outlook.fr>
2022-01-08 14:16:18 -0500

    Change functions DatabaseInterface

--- src/elements/interface/DatabaseInterface.ts
+++ src/elements/interface/DatabaseInterface.ts
@@ -1,24 +1,32 @@
 import Airtable, { FieldSet } from "airtable";
 import { AirtableBase } from "airtable/lib/airtable_base";
-import { IntroductionItem } from "../objects/IntroductionItem";
-
 export abstract class DatabaseInterface{
     
     private static database: AirtableBase = new Airtable({apiKey: 'keyXzoxi702YUSmCf'}).base('applC0kHPlM0YtRDa');
 
-    public static GetIntroductions(introductionData: IntroductionItem[]){
-        if(introductionData.length>0){ introductionData = []; }
+    public static GetIntroductions(data: any[]){
+        if(data.length>0){ data = []; }
         this.database('Introduction').select({
             view: 'Grid view'
         }).firstPage(function(err, records) {
             if (err) { console.error(err); return; }
             if(records){
                 records.forEach(function(record) {
-                    introductionData.push(new IntroductionItem(
-                        record.fields['Title'].toString(), 
-                        record.fields['Text'].toString(),
-                        record.fields['Image'][0]['url'].toString()
-                    ));
+                    data.push(record.fields);
+                });
+            }
+        });
+    }
+    
+    public static GetProjectResume(data: any[]){
+        if(data.length>0){ data = []; }
+        this.database('Projects').select({
+            view: 'Grid view'
+        }).firstPage(function(err, records) {
+            if (err) { console.error(err); return; }
+            if(records){
+                records.forEach(function(record) {
+                    data.push(record.fields);
                 });
             }
         });
